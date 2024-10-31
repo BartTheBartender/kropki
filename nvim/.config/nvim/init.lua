fc52162c45a59cd8cbc517fddd039f5e9b4d26e0
Bartosz Furmanek <126062736+BartTheBartender@users.noreply.github.com>
2024-10-31 15:27:58 +0100

    init.lua splitted

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -25,45 +25,4 @@ local opts = {
 
 }
 
-local plugins = {
-
-  -- catppuccin (mocha) for better colors
-  {
-    "catppuccin/nvim", name = "catppuccin", priority = 1000 
-  },
-
-  -- telescope for file management
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-
-  -- treesitter for AST building while coding
-  {
-    "nvim-treesitter/nvim-treesitter", build= ":TSUpdate"
-  }
-}
-
-require("lazy").setup(plugins, opts)
-
--- require catppuccin
-require("catppuccin").setup()
--- set the colorscheme to it!
-vim.cmd.colorscheme "catppuccin"
-
--- require telescope
-local builtin = require('telescope.builtin')
--- set <C-p> for finding files
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
--- set <leader>fg for greping in files
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-
--- require treesitter
-local config = require("nvim-treesitter.configs")
-config.setup({
-  -- languages to be parsed
-  ensure_installed = {"lua", "javascript", "c", "rust", "haskell", "python"},
-  -- enable highlighting and indents
-  highlight = { enable = true },
-  indent = { enable = true }
-})
+require("lazy").setup("plugins")
