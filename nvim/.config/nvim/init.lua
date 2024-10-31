6f21f8265cebdae481a02a6b9ac4fe906e3d6e54
Bartosz Furmanek <126062736+BartTheBartender@users.noreply.github.com>
2024-10-31 15:40:02 +0100

    added lualine with cattpuccin

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,12 +1,3 @@
--- set tab as two spaces
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-
--- set leading key as space
-vim.g.mapleader= " "
-
 -- set nvim packet manager as lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -25,4 +16,5 @@ local opts = {
 
 }
 
+require("vim-options")
 require("lazy").setup("plugins")
---
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -0,0 +1,9 @@
+-- set tab as two spaces
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+-- set leading key as space
+vim.g.mapleader= " "
+
